6d4056ff49c5bb2d6962b064d22bfb4d1f1e9e0e
Corné Veldman <corne@appbakkers.nl>
2022-10-25 14:40:36 +0200

    Update package skeleton

---
+++ Appbakkers.Authentication/Auth.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Essentials;
+
+namespace Appbakkers.Authentication
+{
+    public class Auth
+    {
+        public static bool Check()
+        {
+            var token = Preferences.Get("access_token", "", "authentication");
+
+            return !string.IsNullOrEmpty(token);
+        }
+
+        public static void Init(string clientId, string clientSecret)
+        {
+            //
+        }
+
+        public static void Login(string username, string password)
+        {
+            Preferences.Set("access_token", "test", "authentication");
+        }
+
+        public static void Logout()
+        {
+            Preferences.Clear("authentication");
+        }
+    }
+}
